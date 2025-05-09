4fc75b36a30518c8afcb3e8383dd190584557d5e
Mark Jack <mark.jack@gmail.com>
2025-05-09 22:59:09 +0100

    Updated workflow

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_app_service" "webapp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  app_service_plan_id = azurerm_service_plan.plan.id
 
   site_config {
     linux_fx_version = "NODE|14-lts"
