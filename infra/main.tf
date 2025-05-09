d50d779f3edad75d37383f8acaf94c7d1c863254
Mark Jack <mark.jack@gmail.com>
2025-05-09 22:45:38 +0100

    Updated workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,42 +1,45 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
+
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "resource_group_name" {}
+variable "location" {}
+variable "app_service_plan_name" {}
+variable "webapp_name" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_service_plan" "plan" {
+resource "azurerm_app_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  kind     = "Linux"
+  reserved = true
 
   sku {
     tier = "Basic"
     size = "B1"
   }
-
-  # for Linux plans
-  reserved = true
 }
 
 resource "azurerm_app_service" "webapp" {
   name                = var.webapp_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
   app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "PYTHON|3.9"
+    linux_fx_version = "NODE|14-lts"
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
 }
