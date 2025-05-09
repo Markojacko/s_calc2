1f4f3e32cc25b85edc470a570001f27b3976a570
Mark Jack <mark.jack@gmail.com>
2025-05-09 23:04:26 +0100

    Updated workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,35 +1,61 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+}
+
+variable "location" {
+  description = "Azure region (e.g. West Europe)"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+}
+
+variable "webapp_name" {
+  description = "Name of the Web App"
+}
+
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
+# App Service Plan (Linux)
 resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  kind     = "Linux"
-  reserved = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type  = "Linux"   # Change to "Windows" for a Windows plan
+  sku_name = "B1"      # e.g. F1, B1, S1, P1v2, etc.
 }
 
-resource "azurerm_app_service" "webapp" {
+# Linux Web App
+resource "azurerm_linux_web_app" "webapp" {
   name                = var.webapp_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  app_service_plan_id = azurerm_service_plan.plan.id
+  service_plan_id = azurerm_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "NODE|14-lts"
+    application_stack {
+      node_version = "14-lts"
+    }
   }
 
   app_settings = {
